a928267b69c83eef7cae41e11c527ad5ebe94920
fwjmath <fwjmath@gmail.com>
2020-06-25 00:22:49 +0200

    Added bijection between nu-Tamari elements and classical tamari elements. Slight refactoring.

--- nu_tamari_lattice.py
+++ nu_tamari_lattice.py
@@ -29,6 +29,49 @@ REFERENCES:
 
 from sage.combinat.posets.lattices import LatticePoset
 
+def checkpath(p):
+    r"""
+    This function checks whether p corresponds to a directed lattice path,
+    i.e., consists of only 0 and 1.
+    
+    INPUT:
+    
+    - ``p`` -- a list of integers
+    
+    OUTPUT: 
+    
+    Nothing, but throws an error when ``p`` has elements other than 0 and 1.
+    """
+    if len(p) != p.count(0) + p.count(1):
+        raise ValueError("The input must be a list of 0 (east steps) and 1 (north step).")
+    return
+
+def get_level_list(v):
+    r"""
+    This function returns the level list of the lattice path ``v``.
+    
+    INPUT:
+    
+    - ``v`` -- a list of integers standing for a directed lattice path
+    
+    OUTPUT: 
+    
+    An array ``lvl`` such that ``lvl[i]`` is the largest abscissa of ordinate 
+    ``i``
+    """
+    p,q = v.count(0), v.count(1)
+    lvl = [0 for i in range(q+1)]
+    tmp = 0
+    ltmp = 0
+    for i in v:
+        if i == 0: 
+            tmp+=1
+        else:
+            lvl[ltmp] = tmp
+            ltmp += 1
+    lvl[q] = p
+    return lvl
+
 def NuTamariLattice(v):
     r"""
     This function returns the `\\nu`-Tamari lattice indexed by ``v``
@@ -40,22 +83,16 @@ def NuTamariLattice(v):
       
     OUTPUT: the `\\nu`-Tamari lattice indexed by ``v``
     """
-    
-    def get_level_list():
-        p,q = v.count(0), v.count(1)
-        lvl = [0 for i in range(q+1)]
-        tmp = 0
-        ltmp = 0
-        for i in v:
-            if i == 0: 
-                tmp+=1
-            else:
-                lvl[ltmp] = tmp
-                ltmp += 1
-        lvl[q] = p
-        return lvl
         
     def extend_path(elem, cur, toextend, x, y):
+        """
+        Extending the path (cur) step by step, within the limit of v 
+        indicated by lvl.
+        
+        The parameter (toextend) is the remaining steps to add.
+        
+        When a path is fully generated, it gets added to (elem).
+        """
         if toextend == 0:
             elem.append(tuple(cur))
             return
@@ -75,6 +112,10 @@ def NuTamariLattice(v):
         return elem
         
     def swap(e, p):
+        """
+        Given a valley p, this function does the v-Tamari switch to obtain a 
+        covering element.
+        """
         x = [e[:p+1].count(0), e[:p+1].count(1)]
         hdist = lvl[x[1]] - x[0]
         for j in range(p+1, len(e)):
@@ -90,7 +131,50 @@ def NuTamariLattice(v):
     def get_cover_elem(e):
         return [swap(e,x) for x in range(len(v)-1) if (e[x] == 0) and (e[x+1] == 1)]
             
-    if len(v) != v.count(0) + v.count(1):
-        raise ValueError("The input must be a list of 0 (east steps) and 1 (north step).")
+    checkpath(v)
     lvl = get_level_list()
     return LatticePoset(dict((e, get_cover_elem(e)) for e in generate_elements()))
+
+def pathpair_to_dyck(u, v):
+    r"""
+    This function converts an pair of lattice paths ``u`` and ``v`` starting
+    and ending at the same point, regarded as an element in Tam(v), into a Dyck
+    path that is the corresponding element in the classical Tamari lattice.
+    
+    INPUT:
+    
+    - ``u``
+    """
+    
+    # Check basic requirements
+    checkpath(u)
+    checkpath(v)
+    if len(u) != len(v):
+        raise ValueError("The two paths should have the same length.")
+    if u.count(0) != v.count(1):
+        raise ValueError("The two paths should end at the same point.")
+    
+    ulvl = get_level_list(u)
+    vlvl = get_level_list(v)
+    h = len(ulvl)
+    
+    # Check whether u is weakly above v
+    for i in range(h):
+        if ulvl[i] > vlvl[i]:
+            raise ValueError("The path u should be weakly above the path v.")
+    
+    # The bijection: type given by v, and descent lengths given by consecuted
+    # east steps in u
+    n = len(u)
+    p = []
+    ptr = 0
+    prev = 0
+    for i in range(n):
+        p.append(1)
+        if v[i] == 1:
+            p += [0] * (ulvl[ptr] - prev + 1)
+            prev = ulvl[ptr]
+            ptr += 1
+    p.append(1)
+    p += [0] * (ulvl[ptr] - prev + 1)
+    return p
